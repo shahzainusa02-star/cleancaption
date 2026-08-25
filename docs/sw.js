bbd603bec763f3b7559976348c6b7c2ac5b7b1cf
shah zain <shahzainusa0.2@gmail.com>
2026-08-25 09:49:46 -0700

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE="captionclean-online-v2-5-fmm-4";
+const CACHE="captionclean-online-v2-5-1-controls-longvideo-1";
 const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));
